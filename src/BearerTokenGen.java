343dc0bc3edc4a776cd93150ed5d160760b26d3b
majkimac <michal.m.baran@gmail.com>
2016-09-12 02:56:12 +0200

    Initial commit

---
+++ src/BearerTokenGen.java
@@ -0,0 +1,32 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * Created by michalbaran on 12/09/16.
+ */
+public class BearerTokenGen {
+
+    private final String consKey = "Gf59eZi1jLpOyf71dPjaGOIyg";
+    private final String consSecret = "TScz6noTduE1HgJ72Fys3CjCKZV1ER4Bk6cy16WCto6eaQ1jHH";
+
+
+    public BearerTokenGen() {
+        // encode keys according to rfc 1738
+
+        String encConsKey = URLEncoder.encodeUTF8(consKey);
+        String ensConsSecret = URLEncoder.encodeUTF8(consSecret);
+
+        System.out.println(encConsKey);
+        System.out.println(ensConsSecret);
+
+        String concKeys = encConsKey + ":" + ensConsSecret;
+        System.out.println(concKeys);
+
+        // base64 encode
+
+        byte[] concByteKeys = concKeys.getBytes(StandardCharsets.UTF_8);
+        String encKeys = Base64.getEncoder().encodeToString(concByteKeys);
+        System.out.println(encKeys);
+    }
+
+}
---
+++ src/TwitTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by michalbaran on 12/09/16.
+ */
+
+public class TwitTest{
+
+
+    public static void main(String[] args){
+
+        new BearerTokenGen();
+        
+
+
+    }
+
+}
